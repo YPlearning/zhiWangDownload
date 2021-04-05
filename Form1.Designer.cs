3938d8d1659849b5e2b594838d83a84d0df90367
YangPing <YYyangping@outlook.com>
2021-04-05 21:03:15 +0800

    第一次测试

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace zhiWangDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(12, 56);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(138, 136);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "生成链接";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(195, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(386, 38);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(195, 56);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(386, 136);
+            this.textBoxOutput.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 198);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(857, 144);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "使用步骤：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(844, 93);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1：右键复制知网整本下载链接\r\n2：粘贴到上方框中\r\n3：点击按钮，等待下载即可（若无法下载复制下载链接粘贴到浏览器中即可）";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 689);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonGenerate);
+            this.Name = "Form1";
+            this.Text = "知网PDF下载链接生成器";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zhiWangDownload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {/**/
+            string input = textBoxInput.Text;
+            string pattern = "&dflag=nhdown&d";
+            string replacement = "&dflag=pdfdown&d";
+            Regex rgx = new Regex(pattern);
+            string result = rgx.Replace(input, replacement);
+            textBoxOutput.Text = result;
+
+
+            RegistryKey key = Registry.ClassesRoot.OpenSubKey(@"http\shell\open\command\");
+            string s = key.GetValue("").ToString();
+            textBoxOutput.Text = s;
+            //s就是你的默认浏览器，不过后面带了参数，把它截去，不过需要注意的是：不同的浏览器后面的参数不一样！
+            //"D:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -- "%1"
+            System.Diagnostics.Process.Start("C:\\Program Files\\Internet Explorer\\iexplore.exe", "http://blog.csdn.net/testcs_dn");
+            //System.Diagnostics.Process.Start("explorer.exe", "https://zhating.cn");
+            //System.Diagnostics.Process.Start("explorer.exe", "https://kns.cnki.net/kcms/download.aspx?filename=rpHOBFDSV1EOvgEZhZkeN9EWtlEchFUVzQmTvYWQ3JlUOhzKJFXOEV1LwMEUZVkNiVGRzc0R2lXSpNHdlB1UJhEZ2smTmFUTppHZLFWO2ZTU4R3cKNUU4ZFNU5Weyp0cJBlUVdXZ2lFUkJmNBVkdLhHcCJ1N3FnS&dflag=pdfdown&dflag=cajdown&tablename=CMFDTEMP&uid=WEEvREcwSlJHSldSdmVqMDh6a1dpb1d5VG9CSzlSZmxyeTlWM1J4U2lWaz0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!");
+        }
+    }
+}
